967ec21b72716d90b3af91239105c29267262b8d
kamocyc <ktgk.chr66g@gmail.com>
2020-01-18 16:35:03 +0900

    余計なファイルを削除

--- src/test1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-
-(async () => {
-  const data = {hoge : [ "huga", "piyo"]};
-  await fs.promises.writeFile('student-2.json', JSON.stringify(data));
-})();
-
